47e42e4cf3f3390b7fb84160ef133056612c7626
Troy Warr <troy3000@gmail.com>
2016-11-15 20:44:17 -0600

    listen for form submit event

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,9 +1,7 @@
-var submit = document.getElementsByClassName('canvas-submit')[0],
-    originParts = document.referrer.split('/'),
-    origin = originParts[0] + '//' + originParts[2],
-    sendMessage = function () {
-      window.parent.postMessage('submitted', origin);
-      console.log('message sent');
-      console.log(origin);
-    };
-submit.addEventListener('canvas_submit', sendMessage);
+$('form').submit(function () {
+  var originParts = document.referrer.split('/'),
+      origin = originParts[0] + '//' + originParts[2];
+  window.parent.postMessage('submitted', origin);
+  console.log('message sent');
+  console.log(origin);
+});
