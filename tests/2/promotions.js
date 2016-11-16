5793a4ce22bde4048d4db189ad1723d0233265bf
Troy Warr <troy3000@gmail.com>
2016-11-15 20:20:58 -0600

    log origin

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,8 +1,8 @@
-var parentURL = (parent !== window) ? document.referrer : document.location,
-    origin = parentURL,
+var origin = (parent !== window) ? document.referrer : document.location,
     submit = document.getElementsByClassName('canvas-submit')[0],
     sendMessage = function () {
       window.parent.postMessage('submitted', origin);
       console.log('message sent');
+      console.log(origin);
     };
 submit.addEventListener('click', sendMessage);
