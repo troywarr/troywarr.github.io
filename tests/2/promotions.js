8314ef7969f135de2a861b18b633786e99a3d0b4
Troy Warr <troy3000@gmail.com>
2016-11-15 20:28:42 -0600

    refine origin

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,5 +1,6 @@
-var origin = (parent !== window) ? document.referrer : document.location,
-    submit = document.getElementsByClassName('canvas-submit')[0],
+var submit = document.getElementsByClassName('canvas-submit')[0],
+    originParts = document.referrer.split('/'),
+    origin = originParts[0] + '//' + originParts[2],
     sendMessage = function () {
       window.parent.postMessage('submitted', origin);
       console.log('message sent');
