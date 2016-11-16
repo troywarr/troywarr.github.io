c5a3ceacec7ce51177a811daffdd3d8d8f7ba9df
Troy Warr <troy3000@gmail.com>
2016-11-15 21:07:23 -0600

    check for loader icon on submit

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,7 +1,20 @@
 $('form').submit(function () {
-  var originParts = document.referrer.split('/'),
-      origin = originParts[0] + '//' + originParts[2];
-  window.parent.postMessage('submitted', origin);
-  console.log('message sent');
-  console.log(origin);
+
+  var originParts = document.referrer.split('/'), // URL of parent page
+      origin = originParts[0] + '//' + originParts[2],
+      $submitIcon = $('.canvas-submit-icon'), // submit button in Promotions form
+      checkSubmitIcon = function () {
+        if ($submitIcon.css('display') === 'block') { // if submit loader is showing
+          window.parent.postMessage('submitted', origin); // send success message to parent page
+          console.log('message sent');
+          console.log(origin);
+        }
+      },
+      interval = setInterval(checkSubmitIcon, 100); // check every 1/10th second
+
+  checkSubmitIcon(); // check initially
+  setTimeout(function () { // check for up to 2 seconds
+    clearInterval(interval);
+  }, 2000);
+
 });
