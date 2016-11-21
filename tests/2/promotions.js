d43f7e974dc23ddd2c19e8953425af8ceeb84f92
Troy Warr <troy3000@gmail.com>
2016-11-21 15:07:05 -0600

    add GA tracking interface

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,26 +1,40 @@
-$('form').submit(function () {
+// notify parent window of form submit
 
+$('form').submit(function () {
   var origin = decodeURIComponent(location.search.slice(1)).split('&')[0],
       $submitIcon = $('.canvas-submit-icon'), // submit button in Promotions form
       checkSubmitIcon = function () {
-        console.log(2);
-        console.log($submitIcon.css('display'));
         if ($submitIcon.css('display') !== 'none') { // if submit loader is showing
-          console.log(origin);
           clearInterval(interval); // stop checking
           top.postMessage('submitted', origin); // send success message to parent page
-          console.log('message sent');
         }
       },
       interval = setInterval(checkSubmitIcon, 200); // check every 1/5th second
-
-  console.log(1);
-  console.log(location);
-  console.log(origin);
 
   checkSubmitIcon(); // check initially
   setTimeout(function () { // check for up to 2 seconds
     clearInterval(interval);
   }, 2000);
+});
 
+// GA tracking
+
+$('input[name="field_4"]').on('click', function (event) { // track submit button click (11 - #3)
+  top.postMessage('tracking|sharePhoto,upload,click');
+});
+
+$('input[name="field_0"]').one('keypress', function (event) { // track submit button click (12 - #2)
+  top.postMessage('tracking|sharePhoto,nativeType,firstName');
+});
+
+$('input[name="field_1"]').one('keypress', function (event) { // track submit button click (12 - #2)
+  top.postMessage('tracking|sharePhoto,nativeType,lastName');
+});
+
+$('textarea[name="field_3"]').one('keypress', function (event) { // track submit button click (12 - #3)
+  top.postMessage('tracking|sharePhoto,nativeType,body');
+});
+
+$('.canvas-submit').on('click', function (event) { // track submit button click (12 - #5)
+  top.postMessage('tracking|sharePhoto,nativeShare,submit');
 });
