5a3df38a674c9abf5efdcbd4990b00a497f7f92b
Troy Warr <troy3000@gmail.com>
2016-11-21 15:11:50 -0600

    enclose in IIFE

--- tests/2/promotions.js
+++ tests/2/promotions.js
@@ -1,40 +1,45 @@
-// notify parent window of form submit
+(function() {
 
-$('form').submit(function () {
-  var origin = decodeURIComponent(location.search.slice(1)).split('&')[0],
-      $submitIcon = $('.canvas-submit-icon'), // submit button in Promotions form
-      checkSubmitIcon = function () {
-        if ($submitIcon.css('display') !== 'none') { // if submit loader is showing
-          clearInterval(interval); // stop checking
-          top.postMessage('submitted', origin); // send success message to parent page
-        }
-      },
-      interval = setInterval(checkSubmitIcon, 200); // check every 1/5th second
+  var origin = decodeURIComponent(location.search.slice(1)).split('&')[0];
 
-  checkSubmitIcon(); // check initially
-  setTimeout(function () { // check for up to 2 seconds
-    clearInterval(interval);
-  }, 2000);
-});
+  // notify parent window of form submit
 
-// GA tracking
+  $('form').submit(function () {
+    var $submitIcon = $('.canvas-submit-icon'), // submit button in Promotions form
+        checkSubmitIcon = function () {
+          if ($submitIcon.css('display') !== 'none') { // if submit loader is showing
+            clearInterval(interval); // stop checking
+            top.postMessage('submitted', origin); // send success message to parent page
+          }
+        },
+        interval = setInterval(checkSubmitIcon, 200); // check every 1/5th second
 
-$('input[name="field_4"]').on('click', function (event) { // track submit button click (11 - #3)
-  top.postMessage('tracking|sharePhoto,upload,click');
-});
+    checkSubmitIcon(); // check initially
+    setTimeout(function () { // check for up to 2 seconds
+      clearInterval(interval);
+    }, 2000);
+  });
 
-$('input[name="field_0"]').one('keypress', function (event) { // track submit button click (12 - #2)
-  top.postMessage('tracking|sharePhoto,nativeType,firstName');
-});
+  // GA tracking
 
-$('input[name="field_1"]').one('keypress', function (event) { // track submit button click (12 - #2)
-  top.postMessage('tracking|sharePhoto,nativeType,lastName');
-});
+  $('input[name="field_4"]').on('click', function (event) { // track submit button click (11 - #3)
+    top.postMessage('tracking|sharePhoto,upload,click', origin);
+  });
 
-$('textarea[name="field_3"]').one('keypress', function (event) { // track submit button click (12 - #3)
-  top.postMessage('tracking|sharePhoto,nativeType,body');
-});
+  $('input[name="field_0"]').one('keypress', function (event) { // track submit button click (12 - #2)
+    top.postMessage('tracking|sharePhoto,nativeType,firstName', origin);
+  });
 
-$('.canvas-submit').on('click', function (event) { // track submit button click (12 - #5)
-  top.postMessage('tracking|sharePhoto,nativeShare,submit');
-});
+  $('input[name="field_1"]').one('keypress', function (event) { // track submit button click (12 - #2)
+    top.postMessage('tracking|sharePhoto,nativeType,lastName', origin);
+  });
+
+  $('textarea[name="field_3"]').one('keypress', function (event) { // track submit button click (12 - #3)
+    top.postMessage('tracking|sharePhoto,nativeType,body', origin);
+  });
+
+  $('.canvas-submit').on('click', function (event) { // track submit button click (12 - #5)
+    top.postMessage('tracking|sharePhoto,nativeShare,submit', origin);
+  });
+
+}());
